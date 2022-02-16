f1c1fd1d221e8baf06000314b28effa1c87f3a3f
Roman <romabogdan147@gmail.com>
2022-02-16 13:05:18 +0300

    feat: add task 'brackets' (Wed Feb 16 13:05:18 RTZ 2022)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arrayFromStr = str.split("");
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let array = bracketsConfig[i];
+
+    for (let j = 0; j < arrayFromStr.length; j++) {
+      if (arrayFromStr[j] == array[0] && arrayFromStr[j + 1] == array[1]) {
+        arrayFromStr.splice(j, 2);
+        j = j - 2;
+        i = -1;
+        if (arrayFromStr.length) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return true;
+};
