907bacf4467a8eb5c5baeab29094de9bfd837f17
Roman <romabogdan147@gmail.com>
2022-02-16 13:08:48 +0300

    fix: fixed task 'brackets' (Wed Feb 16 13:08:48 RTZ 2022)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  let arrayFromStr = str.split("");
+  let arrStr = str.split("");
 
   for (let i = 0; i < bracketsConfig.length; i++) {
-    let array = bracketsConfig[i];
+    let arr = bracketsConfig[i];
 
-    for (let j = 0; j < arrayFromStr.length; j++) {
-      if (arrayFromStr[j] == array[0] && arrayFromStr[j + 1] == array[1]) {
-        arrayFromStr.splice(j, 2);
-        j = j - 2;
+    for (let j = 0; j < arrStr.length; j++) {
+      if (arrStr[j] == arr[0] && arrStr[j + 1] == arr[1]) {
+        arrStr.splice(j, 2);
+        j -= 2;
         i = -1;
-        if (arrayFromStr.length) {
-          return false;
+        if (!arrStr.length) {
+          return true;
         }
       }
     }
   }
 
-  return true;
+  return false;
 };
